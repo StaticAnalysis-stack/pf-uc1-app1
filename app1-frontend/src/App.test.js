c67dfbe1c8e3422bf228d2560c9ee1ecfbf74a8b
Khairdi <211618@NTTDATA.COM>
2021-01-05 22:16:19 +0530

    Adding a test case

--- app1-frontend/src/App.test.js
+++ app1-frontend/src/App.test.js
@@ -1,3 +1,7 @@
+process.env.CI = true
+import React from 'react';
+import { shallow,mount } from 'enzyme';
+import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 import ComponentName from './App';
